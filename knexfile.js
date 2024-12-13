c3744ccd7793366a8e2533cb11e8d544924ae698
Erika Zepeda <157064202+zepedika@users.noreply.github.com>
2024-12-13 12:47:14 -0500

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+require("dotenv").config();
+
+//database_url
+const {
+  DATABASE_URL = "postgresql://we_love_movies_database_user:zThbRqFNNH59bTt9ng0FmMriBxHzuork@dpg-cte70hi3esus73brc7l0-a.oregon-postgres.render.com/we_love_movies_database",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
