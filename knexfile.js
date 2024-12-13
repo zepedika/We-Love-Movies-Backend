5188090b5d6c209ef85acf7eee4a51607f9f67e8
Erika Zepeda <157064202+zepedika@users.noreply.github.com>
2024-12-13 13:03:36 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,10 @@ const {
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      connectionString: postgresql://we_love_movies_database_user:zThbRqFNNH59bTt9ng0FmMriBxHzuork@dpg-cte70hi3esus73brc7l0-a.oregon-postgres.render.com/we_love_movies_database",
+      ssl: { rejectUnauthorized: false },
+    }
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
